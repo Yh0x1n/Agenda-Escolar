e4acec389d11d6658f4553710bdf649d10160dea
yh0x1n <yhoxrossell508@gmail.com>
2024-01-23 20:04:34 -0400

    end of the line

--- Agenda Escolar (by Yhoxin Rossell, Jesús Campos and Guillermo López)/src/main.py
+++ Agenda Escolar (by Yhoxin Rossell, Jesús Campos and Guillermo López)/src/main.py
@@ -28,8 +28,8 @@ try:
 
     cur = conn.cursor()
     
-    cur.execute('drop database if exists agenda_escolar;')
-    cur.execute('create database agenda_escolar;')
+    #cur.execute('drop database if exists agenda_escolar;')
+    #cur.execute('create database if not exists agenda_escolar;')
     cur.execute('use agenda_escolar;')
 
     print('[!] Connected to database!')
@@ -56,7 +56,7 @@ try:
                 'id INT PRIMARY KEY AUTO_INCREMENT,'
                 'nombre VARCHAR(100) NOT NULL,'
                 'descripcion TEXT NULL,'
-                'fecha_entrega DATE NULL,'
+                'fecha_entrega VARCHAR(10) NULL,'
                 'completada BOOLEAN,'
                 'materia_id INT NOT NULL,'
                 'FOREIGN KEY (materia_id) REFERENCES Materias(id)'
@@ -74,16 +74,17 @@ try:
                 ');'
     )
 
-    cur.execute(
+    '''cur.execute(
         'INSERT INTO Materias(nombre, profesor, aula) values'
         "('Auditoría de Sistemas', 'Yocceline Rosillo', 'B2'),"
         "('Defensa Integral VIII', 'Rubén Toyo', 'B2'),"
-        "('Higiene y Seguridad Industrial', 'Robert González', 'C2'),"
+        "('Higiene y Seguridad Industrial', 'Jesús González', 'C2'),"
         "('Marco Legal para el Ejercicio de la Ingeniería', 'Johanna Castillo', 'B2'),"
         "('Teoría de Decisiones', 'Adelina Medina', 'B2'),"
         "('Teleprocesos', 'Argenis Chirino', 'B2, C2'),"
-        "('Tradición Cultura y Folclor Local', 'José Gómez', 'Biblioteca');"
-    )
+        "('Tradición Cultura y Folclor Local', 'José Gómez', 'Biblioteca'),"
+        "('Sistemas Avanzados de Bases de Datos', 'Robert González', 'B2');"
+    )'''
 
 except mariadb.Error as e:
     print(e)
@@ -110,7 +111,7 @@ def v_tareas():
             materia = materia_entry.get()
             id_materia = next(key for key, value in opciones.items() if value == materia)
             description_text = descripcion.get(1.0, "end-1c") 
-            fecha_entrega = cal.get_date()
+            fecha_entrega = str(cal.get_date())
 
             # Ejecuta la sentencia SQL para insertar los datos en la tabla "Tareas"
             cur.execute("INSERT INTO Tareas (nombre, descripcion, fecha_entrega, materia_id) VALUES (?, ?, ?, ?)",
@@ -128,14 +129,15 @@ def v_tareas():
     Tasks_window = Tk()
     Tasks_window.geometry("600x400")
     Tasks_window.title("Agregar Tarea")
+    Tasks_window.config(bg = "light gray")
 
     #Entrada para el nombre de la tarea
-    Label(Tasks_window, text = 'Nombre de la tarea:').pack()
+    Label(Tasks_window, text = 'Nombre de la tarea:', font = "Roboto").pack()
     nombre_tarea = Entry(Tasks_window)
     nombre_tarea.pack()
 
     #Entrada para las materias
-    Label(Tasks_window, text = 'Materia:').pack()
+    Label(Tasks_window, text = 'Materia:', font = "Roboto").pack()
     query = 'select id, nombre from Materias;'
     cur.execute(query)
     result = cur.fetchall()
@@ -144,17 +146,17 @@ def v_tareas():
     materia_entry.pack()
 
     #Entrada para añadir una descripción a la tarea
-    Label(Tasks_window, text = 'Descripción:').pack()
+    Label(Tasks_window, text = 'Descripción:', font = "Roboto").pack()
     descripcion = Text(Tasks_window, height = 1, width = 40)
     descripcion.pack()
 
     #Calendario para establecer la fecha de entrega
-    Label(Tasks_window, text = 'Fecha de entrega:').pack()
+    Label(Tasks_window, text = 'Fecha de entrega:', font = "Roboto").pack()
     cal = Calendar(Tasks_window, selectmode = 'day', day = 12, month = 12, year = 2023)
     cal.pack()
 
     #Botón para guardar los datos
-    Button(Tasks_window, text = 'Guardar', command = guardar_tarea, borderwidth = 3, relief = 'solid').pack()
+    Button(Tasks_window, text = 'Guardar', command = guardar_tarea, borderwidth = 3, relief = 'solid', font = "Roboto").pack()
 
 #Función para agregar una materia
 def v_materias():
@@ -183,29 +185,30 @@ def v_materias():
     Materias_window = Tk()
     Materias_window.geometry("300x200")
     Materias_window.title("Agregar Materia")
+    Materias_window.config(bg  = "light gray")
     
     #Entrada para el nombre de la materia
-    Label(Materias_window, text = 'Nombre de la materia:').pack()
+    Label(Materias_window, text = 'Nombre de la materia:', font = "Roboto").pack()
     nombre_materia = Entry(Materias_window)
     nombre_materia.pack()
 
     #Entrada para el nombre del profesor que da la materia
-    Label(Materias_window, text = 'Nombre del profesor:').pack()
+    Label(Materias_window, text = 'Nombre del profesor:', font = "Roboto").pack()
     nombre_profesor = Entry(Materias_window)
     nombre_profesor.pack()
 
     #Entrada para el aula donde se da la materia
-    Label(Materias_window, text = 'Aula: ').pack()
+    Label(Materias_window, text = 'Aula: ', font = "Roboto").pack()
     nombre_aula = Entry(Materias_window)
     nombre_aula.pack()
 
     #Botón para guardar los cambios
-    Button(Materias_window, text = 'Guardar', borderwidth = 3, relief = 'solid', command = agregar_materia).pack()
+    Button(Materias_window, text = 'Guardar', borderwidth = 3, relief = 'solid', command = agregar_materia, font = "Roboto").pack()
 
 #Función para actualizar la listbox
 def actualizar_listbox():
     #Realizar la consulta para obtener las tareas
-    query = 'SELECT t.id, t.nombre AS tarea, m.nombre AS materia, t.fecha_entrega FROM Tareas t LEFT JOIN Materias m ON t.materia_id = m.id;'
+    query = 'SELECT t.id, t.nombre AS tarea, m.nombre AS materia FROM Tareas t LEFT JOIN Materias m ON t.materia_id = m.id;'
     cur.execute(query)
     result = cur.fetchall()
 
@@ -215,9 +218,7 @@ def actualizar_listbox():
         id_tarea = tarea[0]
         nombre_tarea = tarea[1]
         nombre_materia = tarea[2]
-        fecha_entrega = tarea[3]
-
-        formato = f'{id_tarea} - {nombre_tarea} - {nombre_materia} - Fecha: {fecha_entrega}'
+        formato = f'{id_tarea} - {nombre_tarea} - {nombre_materia}'
         main_list.insert(END, formato)
 
 #Función para eliminar una tarea
@@ -247,8 +248,10 @@ root = Tk()
 root.geometry("600x400")
 root.title("AGENDA ESCOLAR")
 label = Label(root, text = "¡BIENVENIDO A TU AGENDA ESCOLAR!\n"
-              "Echa un vistazo a tus tareas  pendientes.")
-label.place(x = 175, y = 30)
+              "Echa un vistazo a tus tareas pendientes.", bg = "light blue",
+              font = ("Roboto", 12))
+label.place(x = 150, y = 30)
+root.config(bg = "light blue")
 
 #Ventana emergente que muestra los detalles de una tarea agregada
 def detalles(event):
@@ -265,32 +268,33 @@ def detalles(event):
     cur.execute('select t.nombre, m.nombre, m.profesor, m.aula, t.descripcion, t.fecha_entrega from Tareas t join Materias m on t.materia_id = m.id where t.id = %s;', (id_tarea,))
     result = cur.fetchone()
 
-    Label(det, text = f'{result[0]}').pack()
-    Label(det, text = f'Materia: {result[1]}').pack()
-    Label(det, text = f'Profesor: {result[2]}').pack()
-    Label(det, text = f'Aula: {result[3]}').pack()
-    Label(det, text = f'Descripción: {result[4]}').pack()
-    Label(det, text = f'Fecha tope: {result[5]}').pack()
-    Button(det, text = 'Atrás', borderwidth = 3, relief = 'solid', command = salir_detalles).pack()
+    Label(det, text = f'{result[0]}', font = "Roboto").pack()
+    Label(det, text = f'Materia: {result[1]}', font = "Roboto").pack()
+    Label(det, text = f'Profesor: {result[2]}', font = "Roboto").pack()
+    Label(det, text = f'Aula: {result[3]}', font = "Roboto").pack()
+    Label(det, text = f'Descripción: {result[4]}', font = "Roboto").pack()
+    Label(det, text = f'Fecha tope: {result[5]}', font = "Roboto").pack()
+    Button(det, text = 'Atrás', font = "Roboto", borderwidth = 3, relief = 'solid', command = salir_detalles).pack()
 
-main_list = Listbox(root, height = 10, width = 60)
-main_list.place(x = 50, y = 75)
-main_list.bind('<<ListboxSelect>>', detalles)
+main_list = Listbox(root, height = 10, width = 50, borderwidth = 3, relief = "solid")
+main_list.config(font = ("Roboto", 12))
+main_list.place(x = 85, y = 75)
+main_list.bind('<Double-Button-1>', detalles)
 
 #Botones
-btn1 = Button(root, text = "Salir", fg = "black", bg = "white", command = exit, borderwidth = 3, relief = 'solid')
+btn1 = Button(root, text = "Salir", fg = "black", bg = "white", command = exit, borderwidth = 3, relief = 'solid', font = "Roboto")
 btn1.place(x = 400, y = 300, width = 100, height = 50)
 
-btn2 = Button(root, text = "Añadir tareas", fg = "white", bg = "purple", command = v_tareas, borderwidth = 3, relief = 'solid')
-btn2.place(x = 100, y = 300, width = 100)
+btn2 = Button(root, text = "Añadir tareas", fg = "white", bg = "purple", command = v_tareas, borderwidth = 3, relief = 'solid', font = "Roboto")
+btn2.place(x = 82, y = 300, width = 145)
 
-btn3 = Button(root, text = "Eliminar", fg = 'white', bg = 'red', command = eliminar_tarea, borderwidth = 3, relief = "solid")
+btn3 = Button(root, text = "Eliminar", fg = 'white', bg = 'red', command = eliminar_tarea, borderwidth = 3, relief = "solid",  font = "Roboto")
 btn3.place(x = 250, y = 300, width = 100, height = 50)
 
-btn4 = Button(root, text = 'Actualizar', borderwidth = 3, relief = 'solid', command = actualizar_listbox)
+btn4 = Button(root, text = 'Actualizar', borderwidth = 3, relief = 'solid', command = actualizar_listbox, font = "Roboto")
 btn4.pack(side = 'right', anchor = 'sw')
 
-btn5 = Button(root, text = 'Añadir materias', fg = 'white', bg = 'purple', borderwidth = 3, relief = 'solid', command = v_materias)
+btn5 = Button(root, text = 'Añadir materias', fg = 'white', bg = 'purple', borderwidth = 3, relief = 'solid', command = v_materias, font = "Roboto")
 btn5.place(x = 82, y = 335)
 
 #Si se presiona la tecla ESC, se llama a esta función
--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
 # AGENDA ESCOLAR
 Un projecto para una agenda escolar, utilizando MariaDB y Python
+
+UNEFA 2-2023
+SISTEMAS AVANZADOS DE BASES DE DATOS
+
+Autores:
+-Yhoxin Rossell
+-Jesús Campos
+-Guillermo López
